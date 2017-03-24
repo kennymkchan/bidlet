fb127e8bd43f223640aac78c5995d229a84729e5
Kenny Chan <kennykitchan@gmail.com>
2017-03-24 16:44:39 -0400

    Added form validation function to ensure bids are always greater than currPrice

--- api/forms.py
+++ api/forms.py
@@ -1,12 +1,26 @@
 from django import forms
 from datetime import *
-# from .models import Bidding
+from .models import Bidding
 
 class BidForm(forms.Form):
 	bidPrice = forms.DecimalField(label="Your Bid Price", max_digits=8, decimal_places=2)
 
+	def __init__(self, *args, **kwargs):
+		# Passing in the property ID from the form so that we
+		# can ensure we know the current price of the unit
+		self.propertyID = kwargs.pop("propID")
+		super(BidForm, self).__init__(*args, **kwargs)
+
+	def clean_bidPrice(self, **kwargs):
+		bid = self.cleaned_data.get("bidPrice")
+		currentPriceQS = Bidding.objects.filter(propertyID=self.propertyID).first()
+
+		if bid < currentPriceQS.CurPrice + 10:
+			raise forms.ValidationError("Bid must be greater than current bid price by at least $10.00")
+
+		return bid
+
 class CreatePropertyForm(forms.Form):
-	# TODO: Remove the initial
 	title = forms.CharField(initial="4 Room suite sublet for FALL 2017")
 	description = forms.CharField(initial="Best place ever")
 	address = forms.CharField(initial="200 University ave")
@@ -16,7 +30,6 @@ class CreatePropertyForm(forms.Form):
 	suite = forms.IntegerField(initial=400)
 	image = forms.CharField(initial="http://www.hawkswap.com/wp-content/uploads/2012/08/438421.jpg")
 	startPrice = forms.DecimalField(initial=600)
-
 	dateStart = forms.DateTimeField(initial=datetime.now())
 	dateEnd = forms.DateTimeField(initial=datetime.now())
 
